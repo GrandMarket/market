75bd846548c57006d892eb360272fe36cbe42ee3
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-06-21 15:32:28 -0400

    Added help command

--- src/com/github/grandmarket/market/Market.java
+++ src/com/github/grandmarket/market/Market.java
@@ -48,6 +48,7 @@ public class Market extends JavaPlugin implements Listener {
 					}
 					else {
 						sender.sendMessage("The market allows players to buy and sell items.");
+						sender.sendMessage("/"+commandLabel+" help lists avalible commands.");
 						if(sender.hasPermission("market.setup")) {
 							sender.sendMessage("This message is the default help message.  Change the plugin settings (or go through the setup) to change this message.");
 						}
@@ -58,6 +59,31 @@ public class Market extends JavaPlugin implements Listener {
 					getLogger().log(Level.SEVERE, "SQLException onCommand market.main");
 				}
 				return true;
+			}
+		}
+		if(args[0] == "help") {
+			sender.sendMessage("/"+commandLabel+" [commands...]");
+			if(sender.hasPermission("market.main")) {
+				sender.sendMessage("/"+commandLabel+"  Display basic information");
+			}
+			if(sender.hasPermission("market.help")) {
+				sender.sendMessage("/"+commandLabel+" help  Display this information");
+			}
+			if(sender.hasPermission("market.updates")) {
+				sender.sendMessage("/"+commandLabel+" updates Gives personalized updates on the market.");
+				sender.sendMessage("/"+commandLabel+" updates clear  Removes personalized update messages.");
+			}
+			if(sender.hasPermission("market.mystuff")) {
+				sender.sendMessage("/"+commandLabel+" mystuff Displays information about your items on the market.");
+			}
+			if(sender.hasPermission("market.pricecheck")) {
+				sender.sendMessage("/"+commandLabel+" pricecheck <item name>  Checks the price of an item.");
+			}
+			if(sender.hasPermission("market.buy")) {
+				sender.sendMessage("/"+commandLabel+" buy <ammount> <item> ([now [under price]] or [price]) Purchase an item (more information at /" + commandLabel + " buy)");
+			}
+			if(sender.hasPermission("market.sell")) {
+				sender.sendMessage("/"+commandLabel+" sell <ammount> <item> ([now [under price]] or [price])  Sell an item (more info at /" + commandLabel + " sell");
 			}
 		}
 		return false;
